5d9a476160efec406fa121d6ab739ca00100d324
Anguandia <mikeanguandia@gmail.com>
2019-04-17 04:10:32 +0300

    add css for menu icon, background image, footer, menu expansion icon etc

--- UI/static/scripts/main.js
+++ UI/static/scripts/main.js
@@ -14,3 +14,46 @@ function showPage(group, parents){
 
 var index_page = document.getElementsByClassName('index_page');
 index_page.onload = showPage('index', ['menu', 'auth']);
+
+/*slide in and out menu pane to expand, overlapping main content
+on smaller screens*/
+var menu = document.getElementById('menu');
+var icon = document.getElementById('iconcontainer');
+function openMenu(){
+    //only expand on screens smaller than 420px width
+    if(window.innerWidth<620){
+        if(window.innerWidth>window.innerHeight){
+            menu.style.width = '50%';
+        } else {
+            menu.style.height = 'inherit';
+            document.getElementById('options').style.display = 'none';
+        }
+    }
+}
+
+// slide out menu pane
+function closeMenu(){
+    if(window.innerWidth<620 && window.innerWidth>240){
+        if(window.innerWidth>window.innerHeight){
+            menu.style.width = '10%';
+        } else {
+            menu.style.height = '5%';
+            document.getElementById('options').style.display = 'block';
+        }
+    }
+}
+
+//togle display of menu on click of icon
+function showMenu(){
+    menu.style.width = '50%';
+    if(menu.style.display=='block'){
+        menu.style.display = 'none';
+    } else {
+        menu.style.display ='block';
+        openMenu();
+    }
+}
+
+menu.onmouseover = openMenu;
+menu.onmouseout = closeMenu;
+icon.onclick = showMenu;
