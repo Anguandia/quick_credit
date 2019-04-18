601b09b863deb9d239adcad5ee915ca07280d2b5
Anguandia <mikeanguandia@gmail.com>
2019-04-18 06:00:55 +0300

    add custom html validation messages for form fields and remove commented out sections of redundant css

--- UI/static/scripts/auth.js
+++ UI/static/scripts/auth.js
@@ -1,15 +1,15 @@
-document.write(`<form id='signupform'>\
+document.write(`<form id='signupform' action=''>\
 <label class='signup'>first name<span>*</span>\
-<input name='first_name' class='signup' id='first' placeholder='first name' required></input>\
+<input type='text' name='first_name' class='signup' id='first' placeholder='first name' oninvalid='this.setCustomValidity("first name is required")' required></input>\
 </label>\
 <label class='signup'>family name<span>*</span>\
-<input name='family_name' class='signup' id='family_name' placeholder='family name' required></input>\
+<input type='text' name='family_name' class='signup' id='family_name' placeholder='family name' oninvalid='this.setCustomValidity("sur name is required")' required></input>\
 </label>\
 <label class='signin'>user Name<span>*</span>\
-<input name='user_name' class='signin' id='user_name' placeholder='enter user name or email' required></input>\
+<input type='text' name='user_name' class='signin' id='user_name' placeholder='enter user name or email' oninvalid='this.setCustomValidity("either username or email is required to signin")' required></input>\
 </label>\
 <label class='signup'>user Name\
-<input name='user_name' class='signin signup' id='user_name' placeholder='enter user name or email' required></input>\
+<input type='text' name='user_name' class='signin signup' id='user_name' placeholder='enter preferred user name'></input>\
 </label>\
 <label class='signup'>email<span>*</span>\
 <input name='email' type='email' class='signup' id='email' placeholder='email' required></input>\
@@ -18,10 +18,10 @@ document.write(`<form id='signupform'>\
 <input type='mobilePhone' class='signup' id='phone' placeholder='phone number'></input>\
 </label>\
 <label for='password' class='signup signin'>password<span>*</span>\
-<input type='password' class='signup signin' id='password' placeholder='password' required></input>\
+<input type='password' class='signup signin' id='password' placeholder='password' oninvalid='this.setCustomValidity("password is required for login")' required></input>\
 </label>\
 <label for='confirm_password' class='signup'>confirm password<span>*</span>\
-<input type='password' class='signin' id='confirm_password' placeholder='retype password' required></input>\
+<input type='password' class='signin' id='confirm_password' placeholder='retype password' equal='password' oninvalid='this.setCustomValidity("must confirm password")'required></input>\
 </label>\
 <button type='submit' class='signin signup'>submit</button>\
 </form>`);
